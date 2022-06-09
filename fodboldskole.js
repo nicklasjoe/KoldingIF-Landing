b58441c4ef2c9df623dfd639a92d349c2149912a
nicklasjoe <lfcnj@hotmail.com>
2022-06-09 09:41:09 +0200

    soon done

--- fodboldskole.js
+++ fodboldskole.js
@@ -1,9 +1,12 @@
 function scrollWin(x, y) {
 
     if (screen.width < 1024 ) {
-    window.scrollTo(0, 900);
+    window.scrollTo(0, 1000);
+    console.log(1)
 }   else if (screen.width > 1024 && screen.width < 1300) {
     window.scrollTo(0, 1200);
+    console.log(2)
 }   else {
     window.scrollTo(0, 774);
+    console.log(3)
 }}
